47a1d05c2b000b4273067df49f09d4374e1fc12a
Diego Díaz <diefco.web@gmail.com>
2024-05-18 01:00:12 -0500

    Update main.ts with JWT token input and connect button

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,25 @@ import "./style.css";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
-    <h1> Websocket - Client</h1>
+    <h2> Websocket - Client</h2>
+
+    <input id="jwt-token" placeholder="Json Web Token" />
+    <button id="btn-connect">Connect</button>
+
+    <br/>
     <span id="server-status"> Offline </span>
 
     <ul id="clients-ul"></ul>
-  </div>
 `;
 
-connectToServer();
+// connectToServer();
+
+const inputJwt = document.querySelector<HTMLInputElement>("#jwt-token")!;
+const btnConnect = document.querySelector<HTMLButtonElement>("#btn-connect")!;
+
+btnConnect.addEventListener("click", () => {
+  if (inputJwt.value.trim().length <= 0)
+    return alert("Please enter a valid JWT token");
+
+  connectToServer(inputJwt.value.trim());
+});
