33a25e4c4bede4e10c9c9fc67856932a0ee0afc0
Diego Díaz <diefco.web@gmail.com>
2024-05-17 20:20:24 -0500

    add server status indicator to the webpage

--- src/socket-client.ts
+++ src/socket-client.ts
@@ -1,7 +1,21 @@
-import { Manager } from "socket.io-client";
+import { Manager, Socket } from "socket.io-client";
 
 export const connectToServer = () => {
   const manager = new Manager("http://localhost:3000/socket.io/socket.io.js");
 
   const socket = manager.socket("/"); // coneccted to root namespace
+
+  addListeners(socket);
+};
+
+const addListeners = (socket: Socket) => {
+  const serverStatusLabel = document.querySelector("#server-status")!;
+
+  socket.on("connect", () => {
+    serverStatusLabel.textContent = "Online";
+  });
+
+  socket.on("disconnect", () => {
+    serverStatusLabel.textContent = "Offline";
+  });
 };
